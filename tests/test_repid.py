647d5763cc45c7603201b685562d8448d9d64d56
aleksul <aleksandrsulimov@bk.ru>
2021-11-14 15:51:02 +0300

    Add job tests 100% Repid coverage

--- tests/test_repid.py
+++ tests/test_repid.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import pytest
 from aioredis import Redis
 
@@ -18,3 +20,12 @@ async def test_enqueue_job(redis: Redis):
     assert await r._get_all_queues() == ["default"]
     assert await r._get_queued_jobs_ids("default") == [job._id_redis]
     assert await r._get_queued_jobs("default") == [job]
+
+
+@pytest.mark.asyncio()
+async def test_job_expired_but_still_in_queue(redis: Redis):
+    r = Repid(redis)
+    job = await r.enqueue_job("super_job", expires_in=1)
+    assert await r._get_queued_jobs("default") == [job]
+    await asyncio.sleep(1)
+    assert await r._get_queued_jobs("default") == []

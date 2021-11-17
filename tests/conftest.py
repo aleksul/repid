0ad0745188a43e1222778c7ea70435d57ebf5532
Aleksul <sashsasul2003@gmail.com>
2021-11-18 00:26:47 +0300

    Bump version and update Readme

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ import pytest
 
 @pytest.fixture()
 async def redis():
-    redis = await aioredis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
+    redis = aioredis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
     await redis.flushdb()
     yield redis
     await redis.close()

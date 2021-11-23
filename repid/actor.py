8aabed0e7399472e680fded5d115fe4ac424ce90
aleksul <aleksandrsulimov@bk.ru>
2021-11-23 15:33:06 +0300

    Separate Actor decorator in a different class

---
+++ repid/actor.py
@@ -0,0 +1,69 @@
+import asyncio
+import functools
+import re
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+from typing import Any, Callable, Optional
+
+from .job import JobResult
+
+#: This re shows valid actor and queue names.
+_name_re = re.compile(r"[a-zA-Z_][a-zA-Z0-9._-]*")
+
+
+class Actor:
+    def __init__(
+        self,
+        fn: Callable,
+        name: Optional[str] = None,
+        queue: str = "default",
+        retries: int = 1,
+    ):
+        self.fn = fn
+        self.name = name or fn.__name__
+        if not _name_re.fullmatch(self.name):
+            raise ValueError(
+                "Actor name must start with a letter or an underscore"
+                "followed by letters, digits, dashes or underscores."
+            )
+        self.queue = queue
+        if not _name_re.fullmatch(self.queue):
+            raise ValueError(
+                "Queue name must start with a letter or an underscore"
+                "followed by letters, digits, dashes or underscores."
+            )
+        self.retries = retries
+
+    async def __call__(self, *args, **kwargs):
+        result: Any = None
+        success: bool = None
+        started_when = int(datetime.utcnow().timestamp())
+        loop = asyncio.get_event_loop()
+        for _ in range(self.retries):
+            try:
+                if asyncio.iscoroutinefunction(self.fn):
+                    result = await self.fn(*args, **kwargs)
+                else:
+                    with ThreadPoolExecutor() as pool:
+                        result = await loop.run_in_executor(
+                            pool,
+                            functools.partial(self.fn, *args, **kwargs),
+                        )
+            except Exception as exc:
+                result = f"Exception occured while running the job: {type(exc)}: {exc}."
+                success = False
+            else:
+                success = True
+                break
+        return JobResult(
+            success=success,
+            started_when=started_when,
+            finished_when=int(datetime.utcnow().timestamp()),
+            result=result,
+        )
+
+    def __repr__(self):
+        return "Actor(%(fn)r, queue=%(queue)r, name=%(name)r)" % vars(self)
+
+    def __str__(self):
+        return "Actor(%(name)s)" % vars(self)
--- repid/worker.py
+++ repid/worker.py
@@ -1,12 +1,10 @@
 import asyncio
-import functools
-from concurrent.futures import ThreadPoolExecutor
-from datetime import datetime
-from typing import Any, Callable, Dict, Optional, Set
+from typing import Callable, Dict, Optional
 
 import orjson
 from aioredis import Redis
 
+from .actor import Actor
 from .constants import RESULT_PREFIX
 from .job import Job, JobResult
 from .queue import Queue
@@ -14,51 +12,18 @@ from .repid import Repid
 
 
 class Worker(Repid):
-    actors: Dict[str, Callable] = {}
-    _queues: Set[Queue] = set()
+    actors: Dict[str, Actor] = dict()
 
     def __init__(self, redis: Redis):
         self.__redis__ = redis
         super().__init__(redis)
 
     def actor(self, name: Optional[str] = None, queue: str = "default", retries=1):
-        # actually it is a decorator fabric, real decorator is below
+        # actually it is a decorator fabric
+
         def decorator(fn):
-            self._queues.add(Queue(self.__redis__, queue))
-            actor_name = name or fn.__name__
-
-            @functools.wraps(fn)
-            async def wrapper(*args, **kwargs):
-                result: Any = None
-                success: bool = None
-                started_when = int(datetime.utcnow().timestamp())
-                loop = asyncio.get_event_loop()
-                for _ in range(retries):
-                    try:
-                        if asyncio.iscoroutinefunction(fn):
-                            result = await fn(*args, **kwargs)
-                        else:
-                            with ThreadPoolExecutor() as pool:
-                                result = await loop.run_in_executor(
-                                    pool,
-                                    functools.partial(fn, *args, **kwargs),
-                                )
-                    except Exception as exc:
-                        result = f"Exception occured while running the job: {type(exc)}: {exc}."
-                        success = False
-                    else:
-                        success = True
-                        break
-                return JobResult(
-                    success=success,
-                    started_when=started_when,
-                    finished_when=int(datetime.utcnow().timestamp()),
-                    result=result,
-                )
-
-            self.actors[actor_name] = wrapper
-
-            return wrapper
+            a = Actor(fn, name, queue, retries)
+            self.actors[a.name] = a
 
         return decorator
 
@@ -82,7 +47,8 @@ class Worker(Repid):
         await self._save_result(job._id, result)
 
     async def run(self):
-        await asyncio.gather(*[self.run_one_queue(queue) for queue in self._queues])
+        queues = {Queue(self.__redis__, q.queue) for q in list(self.actors.values())}
+        await asyncio.gather(*[self.run_one_queue(q) for q in queues])
 
     async def run_forever(self, delay: float = 1.0):
         while 1:  # pragma: no cover

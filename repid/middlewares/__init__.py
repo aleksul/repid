2d95253f03d7c15c5c937646ca406f321509b20b
aleksul <me@aleksul.space>
2022-08-10 15:42:32 +0000

    ⚡️ logging, typing & perf. in middlewares

--- repid/middlewares/__init__.py
+++ repid/middlewares/__init__.py
@@ -1,8 +1,10 @@
 import asyncio
-import inspect
+import logging
+from asyncio.log import logger
 from functools import partial
+from inspect import getfullargspec, getmembers, isfunction, ismethod
 from itertools import product
-from typing import Any, Callable, Dict, List, Tuple
+from typing import Any, Callable, Coroutine, Dict, List, Tuple, Type
 
 import anyio
 
@@ -26,38 +28,44 @@ POSSIBLE_EVENT_NAMES = set(
 
 
 class Middleware:
-    _events: Dict[str, List[Callable]] = {}
+    _events: Dict[str, List[Callable[..., Coroutine]]] = {}
 
     @classmethod
     def add_event(cls, fn: Callable) -> None:
         name = fn.__name__
         if name in POSSIBLE_EVENT_NAMES:
+            logging.debug(f"Subscribed function with {name = } to middleware signals.")
+
+            # Wrap sync function to make it a coroutine
+            if not asyncio.iscoroutinefunction(fn):
+
+                async def wrapper(*args: Tuple, **kwargs: Dict) -> Any:
+                    return await anyio.to_thread.run_sync(partial(fn, *args, **kwargs))
+
+                fn = wrapper
+
             # add event to the dictionary
             if name not in cls._events:
                 cls._events[name] = []
             cls._events[name].append(fn)
 
     @classmethod
-    def add_middleware(cls, middleware: Any) -> None:
-        for _, fn in inspect.getmembers(middleware, predicate=inspect.ismethod):
+    def add_middleware(cls, middleware: Type[Any]) -> None:
+        for _, fn in getmembers(middleware, predicate=lambda x: isfunction(x) or ismethod(x)):
+            logger.debug(
+                f"Adding event {middleware.__class__.__name__}.{fn.__name__ = } to middleware."
+            )
             cls.add_event(fn)
 
     @classmethod
     async def emit_signal(cls, name: str, kwargs: Dict) -> None:
         if name not in cls._events:
             return
+        logging.debug(f"Emitting signal with {name = }.")
         async with anyio.create_task_group() as tg:
             for fn in cls._events[name]:
-                argspec = inspect.getfullargspec(fn)
-                if not asyncio.iscoroutinefunction(fn):
-
-                    async def wrapper(*args: Tuple, **kwargs: Dict) -> Any:
-                        return await anyio.to_thread.run_sync(
-                            partial(fn, *args, **kwargs)  # noqa: B023
-                        )
-
-                    fn = wrapper
-                kwargs = {
+                argspec = getfullargspec(fn)
+                kwargs = {  # only leave those kwargs that are in the function signature
                     key: value
                     for key, value in kwargs.items()
                     if key in argspec.args

89128b9d889c62649cd6acb6982e3460a65656cd
Alex <me@aleksul.space>
2022-08-16 14:25:39 +0000

    👌 IMPROVE: __conn -> _conn

--- repid/__init__.py
+++ repid/__init__.py
@@ -1,5 +1,7 @@
-from .connection import Connection
+from .connection import BUCKETINGS_MAPPING, CONNECTIONS_MAPPING, Connection
+from .data import ArgsBucket, ResultBucket
 from .job import Job
 from .main import Repid
 from .queue import Queue
+from .serializer import BucketSerializer, MessageSerializer
 from .worker import Worker

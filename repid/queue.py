725f083635e00c6d16c2873d92297869773b9c39
Alex <me@aleksul.space>
2022-08-16 14:25:39 +0000

    🚚 rename __conn -> _conn

--- repid/queue.py
+++ repid/queue.py
@@ -6,7 +6,7 @@ from repid.utils import VALID_NAME
 
 
 class Queue:
-    __slots__ = ("name", "__conn")
+    __slots__ = ("name", "_conn")
 
     def __init__(
         self,
@@ -20,16 +20,16 @@ class Queue:
                 "followed by letters, digits, dashes or underscores."
             )
 
-        self.__conn = _connection or Repid.get_default_connection()
+        self._conn = _connection or Repid.get_default_connection()
 
     async def declare(self) -> None:
-        await self.__conn.messager.queue_declare(self.name)
+        await self._conn.messager.queue_declare(self.name)
 
     async def flush(self) -> None:
-        await self.__conn.messager.queue_flush(self.name)
+        await self._conn.messager.queue_flush(self.name)
 
     async def delete(self) -> None:
-        await self.__conn.messager.queue_delete(self.name)
+        await self._conn.messager.queue_delete(self.name)
 
     def __str__(self) -> str:
         return f"Queue({self.name})"

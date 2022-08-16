32b7755fabe81da2753922a04fb355aaf609cf22
Alex <me@aleksul.space>
2022-08-16 14:27:24 +0000

    ⚡️remove limiter in worker

--- repid/worker.py
+++ repid/worker.py
@@ -1,14 +1,15 @@
+from __future__ import annotations
+
 import logging
 import signal
-from typing import Callable, Dict, FrozenSet, Union
-from uuid import UUID, uuid4
+from itertools import cycle
+from typing import Callable
 
 import anyio
 
 from repid.actor import Actor
 from repid.connection import Connection
-from repid.data import ArgsBucket, Message, ResultBucket
-from repid.data.messages import ArgsBucketMetadata, SimpleArgsBucket
+from repid.data import Args, ArgsBucket, Message, ResultBucket
 from repid.main import Repid
 from repid.utils import unix_time
 
@@ -17,35 +18,34 @@ logger = logging.getLogger(__name__)
 
 class Worker:
     __slots__ = (
-        "__conn",
+        "_conn",
         "actors",
-        "limiter",
         "gracefull_shutdown_time",
-        "limit_processed_messages_amount",
-        "_processed_messages_amount",
+        "messages_limit",
+        "_processed",
     )
 
     def __init__(
         self,
-        limit: int = 16,
         gracefull_shutdown_time: float = 25.0,
-        limit_processed_messages_amount: int = float("inf"),  # type: ignore[assignment]
-        _connection: Union[Connection, None] = None,
+        messages_limit: int = float("inf"),  # type: ignore[assignment]
+        _connection: Connection | None = None,
     ):
-        self.__conn = _connection or Repid.get_default_connection()
-        self.actors: Dict[str, Actor] = {}
-        self.limiter = anyio.CapacityLimiter(limit)
+        self._conn = _connection or Repid.get_default_connection()
+        self.actors: dict[str, Actor] = {}
         self.gracefull_shutdown_time = gracefull_shutdown_time
-        self._processed_messages_amount = 0
-        self.limit_processed_messages_amount = limit_processed_messages_amount
+        self._processed = 0
+        self.messages_limit = messages_limit
 
     @property
-    def topics(self) -> FrozenSet[str]:
+    def topics(self) -> frozenset[str]:
         return frozenset(self.actors.keys())
 
-    def actor(self, name: Union[str, None] = None, queue: str = "default") -> Callable:
-        """Decorator fabric."""
+    @property
+    def messages_processed(self) -> int:
+        return self._processed
 
+    def actor(self, name: str | None = None, queue: str = "default") -> Callable:
         def decorator(fn: Callable) -> Callable:
             a = Actor(fn, name=name, queue=queue)
             self.actors[a.name] = a
@@ -53,98 +53,117 @@ class Worker:
 
         return decorator
 
-    async def __get_message_args(
-        self, message: Message
-    ) -> Union[ArgsBucket, SimpleArgsBucket, None]:
-        if message.args_bucket is None:
-            return None
-        elif isinstance(message.args_bucket, SimpleArgsBucket):
-            return message.args_bucket
-        elif isinstance(message.args_bucket, ArgsBucketMetadata):
-            if self.__conn.args_bucketer is None:
+    async def _get_message_args(self, message: Message) -> Args:
+        if message.simple_args or message.simple_kwargs:
+            return Args(message.simple_args or (), message.simple_kwargs or {})
+        elif message.args_bucket_id:
+            if self._conn.args_bucketer is None:
                 raise ConnectionError("No args bucketer provided.")
-            bucket = await self.__conn.args_bucketer.get_bucket(message.args_bucket.id_)
+            bucket = await self._conn.args_bucketer.get_bucket(message.args_bucket_id)
             if bucket is None:
-                logger.error(f"No bucket found for id = {message.args_bucket.id_}.")
+                logger.error(f"No bucket found for id = {message.args_bucket_id}.")
             if isinstance(bucket, ArgsBucket):
-                return bucket
-        return None
+                return Args(bucket.args or (), bucket.kwargs or {})
+        return Args((), {})
 
-    async def __set_message_result(self, result: ResultBucket) -> None:
-        if self.__conn.results_bucketer is None:
+    async def _set_result(self, result: ResultBucket) -> None:
+        if self._conn.results_bucketer is None:
             logger.error("No results bucketer provided for returned data.")
             return
-        await self.__conn.results_bucketer.store_bucket(result)
+        await self._conn.results_bucketer.store_bucket(result)
 
     async def _process_message(self, message: Message) -> None:
         actor = self.actors[message.topic]
-        args_bucket = await self.__get_message_args(message)
-        args = getattr(args_bucket, "args", ())
-        kwargs = getattr(args_bucket, "kwargs", {})
+        args, kwargs = await self._get_message_args(message)
 
         actor._TIME_LIMIT.set(message.execution_timeout)
         result = await actor(*args, **kwargs)
 
         # rescheduling (retry)
         if not result.success and message.tried + 1 < message.retries:
-            message.tried += 1
+            message._increment_retry()
             # message.delay_until = None  # TODO: exponential backoff
-            await self.__conn.messager.requeue(message)
+            await self._conn.messager.requeue(message)
         # rescheduling (deferred)
         elif message.defer_by is not None or message.cron is not None:
             message._prepare_reschedule()
-            await self.__conn.messager.requeue(message)
+            await self._conn.messager.requeue(message)
         # ack
         elif result.success:
-            await self.__conn.messager.ack(message)
+            await self._conn.messager.ack(message)
         # nack
         else:
-            await self.__conn.messager.nack(message)
+            await self._conn.messager.nack(message)
 
         # return result
-        if message.result_bucket is not None:
+        if message.result_bucket_id is not None:
             result_with_metadata = dict(
-                id_=message.result_bucket.id_,
-                ttl=message.result_bucket.ttl,
+                id_=message.result_bucket_id,
+                ttl=message.result_bucket_ttl,
                 timestamp=unix_time(),
                 **result._asdict(),
             )
-            await self.__set_message_result(ResultBucket(**result_with_metadata))
+            await self._set_result(ResultBucket(**result_with_metadata))
 
-    async def __process_message_with_limiter(self, message: Message, task_id: UUID) -> None:
+    async def __process_message_with_cancellation(self, message: Message) -> None:
         try:
             await self._process_message(message)
         except anyio.get_cancelled_exc_class():
-            await self.__conn.messager.reject(message)
+            with anyio.CancelScope(shield=True):
+                await self._conn.messager.reject(message)
             raise
-        finally:
-            self.limiter.release_on_behalf_of(task_id)
+        return None
 
-    async def __listen_signal(self, scope: anyio.CancelScope) -> None:
+    @staticmethod
+    async def __stop_on_event(
+        scope: anyio.CancelScope,
+        on_event: anyio.Event,
+    ) -> None:
+        await on_event.wait()
+        scope.cancel()
+
+    @staticmethod
+    async def __listen_signal(
+        scope: anyio.CancelScope,
+        on_event: anyio.Event,
+        shutdown_time: float,
+    ) -> None:
         with anyio.open_signal_receiver(signal.SIGINT, signal.SIGTERM) as signals:
             await signals.__anext__()
-            self.limiter.total_tokens *= -1
-            await anyio.sleep(self.gracefull_shutdown_time)
+            on_event.set()
+            await anyio.sleep(shutdown_time)
             scope.cancel()
+
+    async def __listen_signal_with_stop(
+        self,
+        scope: anyio.CancelScope,
+        on_stop_consume: anyio.Event,
+    ) -> None:
+        async with anyio.create_task_group() as tg:
+            tg.start_soon(
+                self.__stop_on_event,
+                tg.cancel_scope,
+                on_stop_consume,
+            )
+            tg.start_soon(
+                self.__listen_signal,
+                scope,
+                on_stop_consume,
+                self.gracefull_shutdown_time,
+            )
 
     async def run(self) -> None:
         queue_names = {q.queue for q in self.actors.values()}
         topics_by_queue = {
             q: frozenset(t for t in self.topics if self.actors[t].queue == q) for q in queue_names
         }
+        queue_iter = cycle(queue_names)
+        stop_consume_event = anyio.Event()
         async with anyio.create_task_group() as tg:
-            tg.start_soon(self.__listen_signal, tg.cancel_scope)
-            while self.limit_processed_messages_amount > self._processed_messages_amount:
-                for queue_name in queue_names:
-                    if not self.limit_processed_messages_amount > self._processed_messages_amount:
-                        break
-                    task_id = uuid4()
-                    await self.limiter.acquire_on_behalf_of(task_id)
-                    message = await self.__conn.messager.consume(
-                        queue_name, topics_by_queue[queue_name]
-                    )
-                    self._processed_messages_amount += 1
-                    tg.start_soon(self.__process_message_with_limiter, message, task_id)
-            while self.limiter.borrowed_tokens > 0:  # Let the tasks finish
-                await anyio.sleep(1.0)
-            tg.cancel_scope.cancel()
+            tg.start_soon(self.__listen_signal_with_stop, tg.cancel_scope, stop_consume_event)
+            while not stop_consume_event.is_set() and self.messages_limit > self._processed:
+                queue = next(queue_iter)
+                message = await self._conn.messager.consume(queue, topics_by_queue[queue])
+                tg.start_soon(self.__process_message_with_cancellation, message)
+                self._processed += 1
+            stop_consume_event.set()

f35b0324cf94aba8f4a40d75afd809a5d6ea4f36
Alex <me@aleksul.space>
2022-09-05 12:09:35 +0200

    🚀 RELEASE: v1.0.0b11 (#56)
    * 🚸 Update module imports
    * ✅ Add retry test
    * 💥 Remove declare_queue arg in Job.enqueue
    * 🚸 Add run_in_process flag to an actor

--- repid/__init__.py
+++ repid/__init__.py
@@ -1,8 +1,9 @@
-from .actor import ActorResult
+from .actor import DEFAULT_RETRY_POLICY, Actor, ActorResult
 from .connection import BUCKETINGS_MAPPING, CONNECTIONS_MAPPING, Connection
 from .data import ArgsBucket, PrioritiesT, ResultBucket, ResultMetadata
 from .job import Job
 from .main import Repid
 from .queue import Queue
+from .retry_policy import RetryPolicyT, default_retry_policy
 from .serializer import BucketSerializer, MessageSerializer
 from .worker import Worker

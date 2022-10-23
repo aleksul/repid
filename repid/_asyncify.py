8db9f2b7988d4ca6c686f94f5ad7f58973e3b079
aleksul <me@aleksul.space>
2022-10-23 18:37:10 +0200

    🏷️ Better asyncify decorator with overload

--- repid/_asyncify.py
+++ repid/_asyncify.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
+
 import asyncio
 import sys
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 from functools import partial, wraps
-from typing import Any, Callable, Coroutine, TypeVar
+from typing import Any, Callable, Coroutine, TypeVar, overload
 
 if sys.version_info >= (3, 10):
     from typing import ParamSpec
@@ -13,13 +15,29 @@ FnP = ParamSpec("FnP")
 FnR = TypeVar("FnR")
 
 
+@overload
+def asyncify(
+    fn: Callable[FnP, Coroutine[Any, Any, FnR]],
+    run_in_process: bool = False,
+) -> Callable[FnP, Coroutine[Any, Any, FnR]]:
+    ...
+
+
+@overload
 def asyncify(
     fn: Callable[FnP, FnR],
     run_in_process: bool = False,
 ) -> Callable[FnP, Coroutine[Any, Any, FnR]]:
+    ...
+
+
+def asyncify(
+    fn: Callable[FnP, FnR] | Callable[FnP, Coroutine[Any, Any, FnR]],
+    run_in_process: bool = False,
+) -> Callable[FnP, Coroutine[Any, Any, FnR]]:
 
     if asyncio.iscoroutinefunction(fn):
-        return fn  # type: ignore[return-value]
+        return fn
 
     executor = ProcessPoolExecutor if run_in_process else ThreadPoolExecutor
 
@@ -27,6 +45,9 @@ def asyncify(
     async def inner(*args: FnP.args, **kwargs: FnP.kwargs) -> FnR:
         loop = asyncio.get_running_loop()
         with executor() as pool:
-            return await loop.run_in_executor(pool, partial(fn, *args, **kwargs))
+            return await loop.run_in_executor(
+                pool,
+                partial(fn, *args, **kwargs),  # type: ignore[arg-type]
+            )
 
     return inner

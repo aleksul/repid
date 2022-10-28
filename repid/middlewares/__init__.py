10e15e91d7900ab787b2f49d295b2067b6b9724a
aleksul <me@aleksul.space>
2022-10-28 02:58:00 +0200

    ♻️ Better middleware namings, docs and logs

--- repid/middlewares/__init__.py
+++ repid/middlewares/__init__.py
@@ -1,3 +1,3 @@
-from .consts import AVAILABLE_FUNCTIONS, POSSIBLE_EVENT_NAMES
+from .consts import WRAPPED, SUBSCRIBERS_NAMES
 from .middleware import Middleware as Middleware
 from .wrapper import middleware_wrapper as middleware_wrapper

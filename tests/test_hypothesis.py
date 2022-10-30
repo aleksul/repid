72f9f1915f5c7821187f1e4f73b7a92a7c1b436d
aleksul <me@aleksul.space>
2022-10-30 17:04:26 +0100

    ✅ Re-enable hypothesis test

--- tests/test_hypothesis.py
+++ tests/test_hypothesis.py
@@ -1,102 +1,99 @@
-"""
 from datetime import datetime, timedelta
+from functools import partial
+from string import printable
 
+import pytest
 from hypothesis import HealthCheck, given, settings
 from hypothesis.strategies import (
     booleans,
     builds,
-    characters,
     datetimes,
     dictionaries,
     floats,
     from_regex,
     integers,
+    just,
+    lists,
     none,
     one_of,
+    recursive,
     sampled_from,
     text,
     timedeltas,
-    tuples,
 )
 
-from repid import ArgsBucket, Job, PrioritiesT, Queue, ResultMetadata
+from repid import Job, PrioritiesT, Queue
 from repid.utils import VALID_ID, VALID_NAME
 
 MAX = 2**31 - 1
 MIN = -(2**31)
 
-
-def queue_st():
-    return builds(
-        Queue,
-        name=from_regex(VALID_NAME, fullmatch=True),
-        _connection=none(),
-    )
+json_st = recursive(
+    none() | booleans() | floats() | text(printable),
+    lambda children: lists(children) | dictionaries(text(printable), children),
+)
 
 
-def jsonable():
-    return one_of(
-        integers(min_value=MIN, max_value=MAX),
-        floats(min_value=MIN, max_value=MAX),
+queue_st = builds(
+    Queue,
+    name=from_regex(VALID_NAME, fullmatch=True),
+    _connection=none(),
+)
+
+
+unfinished_job_st = partial(
+    builds,
+    Job,
+    name=from_regex(VALID_NAME, fullmatch=True),
+    queue=one_of(from_regex(VALID_NAME, fullmatch=True), queue_st),
+    priority=sampled_from(PrioritiesT),
+    id_=one_of(from_regex(VALID_ID, fullmatch=True), none()),
+    deferred_until=one_of(datetimes(), none()),
+    retries=integers(min_value=1, max_value=MAX),
+    timeout=timedeltas(min_value=timedelta(seconds=1)),
+    ttl=one_of(timedeltas(min_value=timedelta(seconds=1)), none()),
+    args_id=one_of(from_regex(VALID_ID, fullmatch=True), none()),
+    args_ttl=one_of(timedeltas(min_value=timedelta(seconds=1)), none()),
+    args=one_of(none(), json_st),
+    use_args_bucketer=one_of(none(), booleans()),
+    result_id=one_of(from_regex(VALID_ID, fullmatch=True), none()),
+    result_ttl=one_of(timedeltas(min_value=timedelta(seconds=1)), none()),
+    store_result=one_of(none(), booleans()),
+    _connection=none(),
+)
+
+deferred_by_job_st = unfinished_job_st(
+    deferred_by=one_of(
+        timedeltas(
+            min_value=timedelta(seconds=1),
+            max_value=datetime.max - datetime.now(),
+        ),
         none(),
-        datetimes(),
-        text(),
-    )
+    ),
+    cron=none(),
+)
+
+cron_job_st = unfinished_job_st(
+    deferred_by=none(),
+    cron=one_of(
+        just("5 4 * * *"),
+        just("0 22 * * 1-5"),
+        just("23 0-20/2 * * *"),
+        none(),
+    ),
+)
+
+job_st = one_of(deferred_by_job_st, cron_job_st)
 
 
-def job_args_bucket_st():
-    return builds(
-        ArgsBucket,
-        id_=from_regex(VALID_ID, fullmatch=True),
-        args=one_of(none(), tuples(jsonable())),
-        kwargs=one_of(none(), dictionaries(keys=characters(), values=jsonable())),
-        timestamp=integers(min_value=0, max_value=MAX),
-        ttl=one_of(integers(min_value=1, max_value=MAX), none()),
-    )
-
-
-def job_result_metadata_st():
-    return builds(
-        ResultMetadata,
-        id_=from_regex(VALID_ID, fullmatch=True),
-        ttl=one_of(integers(min_value=1, max_value=MAX), none()),
-    )
-
-
-def job_st():
-    return builds(
-        Job,
-        name=from_regex(VALID_NAME, fullmatch=True),
-        queue=one_of(
-            from_regex(VALID_NAME, fullmatch=True),
-            queue_st(),
-        ),
-        priority=sampled_from(PrioritiesT),
-        deferred_until=one_of(
-            datetimes(min_value=datetime(1970, 1, 1)),
-            integers(min_value=0, max_value=MAX),
-            none(),
-        ),
-        deferred_by=one_of(
-            timedeltas(min_value=timedelta(seconds=0)),
-            integers(min_value=0, max_value=MAX),
-            none(),
-        ),
-        retries=integers(min_value=1, max_value=MAX),
-        timeout=integers(min_value=1, max_value=MAX),
-        ttl=one_of(integers(min_value=1, max_value=MAX), none()),
-        id_=one_of(from_regex(VALID_ID, fullmatch=True), none()),
-        args=one_of(none(), job_args_bucket_st()),
-        use_args_bucketer=one_of(none(), booleans()),
-        result_metadata=one_of(none(), job_result_metadata_st()),
-        _connection=none(),
-    )
-
-
-@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
-@given(job=job_st())
-async def test_job_creation(fake_connection, job):
+@settings(
+    suppress_health_check=[HealthCheck.function_scoped_fixture],
+    deadline=timedelta(milliseconds=750),
+    max_examples=1000,
+)
+@given(job=job_st)
+@pytest.mark.usefixtures("fake_connection")
+async def test_job_creation(job: Job) -> None:
     assert job
     await job.queue.declare()
     await job.enqueue()
-"""

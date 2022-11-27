8a327ac06ac55c2aeffdb56f3f2edfb561608808
aleksul <me@aleksul.space>
2022-11-27 01:46:25 +0100

    ✅ Update tests

---
+++ tests/test_serializer.py
@@ -0,0 +1,10 @@
+from repid.job import Job
+from repid.serializer import MessageSerializer
+
+
+def test_simple_message(fake_connection):
+    j = Job("my-awesome-job")
+    msg = j._to_message()
+    encoded = MessageSerializer.encode(msg)
+    decoded = MessageSerializer.decode(encoded)
+    assert msg == decoded

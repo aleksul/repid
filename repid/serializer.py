c4932f27265a37b1e4dca6f378317a8051fb9e69
aleksul <me@aleksul.space>
2023-07-01 11:31:22 +0200

    ⬆️ Update to Pydantic v2 stable

--- repid/serializer.py
+++ repid/serializer.py
@@ -1,12 +1,9 @@
-from dataclasses import asdict, is_dataclass
 from typing import Any, Protocol
 
 from repid._utils import JSON_ENCODER, is_installed
 
-if PYDANTIC_IMPORTED := is_installed("pydantic"):
+if is_installed("pydantic"):
     import pydantic
-
-    PYDANTIC_V2 = is_installed("pydantic", ">=2.0.0a1,<3.0.0")
 
 
 class SerializerT(Protocol):
@@ -15,10 +12,8 @@ class SerializerT(Protocol):
 
 
 def default_serializer(data: Any) -> str:
-    if PYDANTIC_IMPORTED and isinstance(data, pydantic.BaseModel):
-        if PYDANTIC_V2:
+    if is_installed("pydantic") and isinstance(data, pydantic.BaseModel):
+        if is_installed("pydantic", ">=2.0.0,<3.0.0"):
             return data.model_dump_json()
         return data.json()  # pragma: no cover
-    if is_dataclass(data) and not isinstance(data, type):
-        return JSON_ENCODER.encode(asdict(data))
     return JSON_ENCODER.encode(data)

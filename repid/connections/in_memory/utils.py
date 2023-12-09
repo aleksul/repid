277715965b525e4d1d0ffdfe62d29908df027bfc
aleksul <me@aleksul.space>
2023-12-09 20:08:37 +0100

    🏷️ Properly annotate dummy queue

--- repid/connections/in_memory/utils.py
+++ repid/connections/in_memory/utils.py
@@ -1,6 +1,8 @@
+from __future__ import annotations
+
 import asyncio
 from dataclasses import dataclass, field
-from typing import TYPE_CHECKING, Dict, List, Set, Union
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from datetime import datetime
@@ -10,20 +12,20 @@ if TYPE_CHECKING:
 
 @dataclass(frozen=True)
 class Message:
-    key: "RoutingKeyT"
+    key: RoutingKeyT
     payload: str
-    parameters: "ParametersT"
+    parameters: ParametersT
 
 
 @dataclass
 class DummyQueue:
-    simple: asyncio.Queue = field(default_factory=asyncio.Queue)
-    delayed: Dict["datetime", List[Message]] = field(default_factory=dict)
-    dead: List[Message] = field(default_factory=list)
-    processing: Set[Message] = field(default_factory=set)
+    simple: asyncio.Queue[Message] = field(default_factory=asyncio.Queue)
+    delayed: dict[datetime, list[Message]] = field(default_factory=dict)
+    dead: list[Message] = field(default_factory=list)
+    processing: set[Message] = field(default_factory=set)
 
 
-def wait_until(params: Union["ParametersT", None] = None) -> Union["datetime", None]:
+def wait_until(params: ParametersT | None = None) -> datetime | None:
     if params is None or params.delay is None:
         return None
     return params.delay.next_execution_time or params.compute_next_execution_time

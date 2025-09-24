9a200aa3f47869ad71c19919e17cf7989f8532a1
aleksul <me@aleksul.space>
2025-09-25 00:26:51 +0200

    🚧 Get dependency

--- repid/_utils/get_dependency.py
+++ repid/_utils/get_dependency.py
@@ -1,23 +1,15 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Annotated, Any, cast, get_origin
+from typing import Annotated, Any, get_origin
 
-if TYPE_CHECKING:
-    from repid.dependencies.protocols import DependencyT
+from repid.dependencies.protocols import DependencyT
 
 
 def get_dependency(t: Any) -> DependencyT | None:
     # check if type is directly a depenendency
-    if getattr(t, "__repid_dependency__", "") == "direct":
-        return cast("DependencyT", t)
+    if isinstance(t, DependencyT):
+        return t
     # check if dependency is inside of Annotated
-    if get_origin(t) is Annotated and (
-        getattr(
-            dep := t.__metadata__[0],
-            "__repid_dependency__",
-            "",
-        )
-        == "annotated"
-    ):
-        return cast("DependencyT", dep)
+    if get_origin(t) is Annotated and isinstance(dep := t.__metadata__[0], DependencyT):
+        return dep
     return None

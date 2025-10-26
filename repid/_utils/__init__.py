8671a0f5f657f3cfc990435debb81c838596b406
aleksul <me@aleksul.space>
2025-10-26 13:08:56 +0100

    ⚰️ Remove internal exceptions util

--- repid/_utils/__init__.py
+++ repid/_utils/__init__.py
@@ -1,4 +1,3 @@
 from .asyncify_ import asyncify as asyncify
-from .internal_exceptions import _NoAction as _NoAction
 from .is_installed import is_installed as is_installed
 from .json_encoder import JSON_ENCODER as JSON_ENCODER

02d7f60e7dbd535dceb11a09367b9c80aae32889
aleksul <me@aleksul.space>
2025-11-28 09:54:12 +0100

    ✅ Fix integration fixtures

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,39 +1,32 @@
+from pathlib import Path
 from time import sleep
-from typing import TYPE_CHECKING, Any
+from typing import Any
 
 import pytest
-from pytest_docker_tools import container
+from pytest_docker_tools import container, wrappers
 from pytest_lazy_fixtures import lf as lazy_fixture
 
-from repid import Connection, InMemoryBucketBroker, InMemoryMessageBroker, Repid
-from repid.connections import RabbitMessageBroker, RedisBucketBroker, RedisMessageBroker
+from repid import Repid
+from repid.connections.amqp import AmqpServer
 
-if TYPE_CHECKING:
-    from pytest_docker_tools import wrappers
+RABBITMQ_DEFINITIONS_JSON = Path(__file__).parent / "rabbitmq_definitions.json"
+
 
 redis_container = container(
-    image="redis:7.2-alpine",
+    image="redis:8.4-alpine",
     ports={"6379/tcp": None},
     command="redis-server --requirepass test",
     scope="session",
 )
 
 rabbitmq_container = container(
-    image="rabbitmq:3.12-alpine",
+    image="rabbitmq:4.2-alpine",
     ports={"5672/tcp": None},
-    environment={
-        "RABBITMQ_DEFAULT_USER": "user",
-        "RABBITMQ_DEFAULT_PASS": "testtest",
-    },
-    scope="session",
-)
-
-rabbitmq_container_2 = container(
-    image="rabbitmq:3.12-alpine",
-    ports={"5672/tcp": None},
-    environment={
-        "RABBITMQ_DEFAULT_USER": "user",
-        "RABBITMQ_DEFAULT_PASS": "testtest",
+    volumes={
+        str(RABBITMQ_DEFINITIONS_JSON): {
+            "bind": "/etc/rabbitmq/definitions.json",
+            "mode": "ro",
+        },
     },
     scope="session",
 )
@@ -41,84 +34,43 @@ rabbitmq_container_2 = container(
 
 @pytest.fixture(scope="session")
 def rabbitmq_connection(rabbitmq_container: "wrappers.Container") -> Repid:
-    while not rabbitmq_container.ready():
+    while (
+        not rabbitmq_container.ready()
+        and "Server startup complete" not in rabbitmq_container.logs()
+    ):
         sleep(0.1)
-    return Repid(
-        Connection(
-            RabbitMessageBroker(
-                f"amqp://user:testtest@localhost:{rabbitmq_container.ports['5672/tcp'][0]}",
-            ),
-        ),
+
+    rabbitmq_container.exec_run("rabbitmqctl import_definitions /etc/rabbitmq/definitions.json")
+
+    while "Successfully set permissions for user 'user'" not in rabbitmq_container.logs():
+        sleep(0.1)
+
+    server = AmqpServer(
+        dsn=f"amqp://user:testtest@localhost:{rabbitmq_container.ports['5672/tcp'][0]}",
     )
+
+    app = Repid()
+    app.servers.register_server("default", server, is_default=True)
+
+    return app
 
 
 @pytest.fixture(scope="session")
 def redis_connection(redis_container: "wrappers.Container") -> Repid:
     while not redis_container.ready():
         sleep(0.1)
-    return Repid(
-        Connection(
-            RedisMessageBroker(f"redis://:test@localhost:{redis_container.ports['6379/tcp'][0]}/0"),
-            RedisBucketBroker(f"redis://:test@localhost:{redis_container.ports['6379/tcp'][0]}/1"),
-            RedisBucketBroker(
-                f"redis://:test@localhost:{redis_container.ports['6379/tcp'][0]}/1",
-                use_result_bucket=True,
-            ),
-        ),
-    )
-
-
-@pytest.fixture(scope="session")
-def rabbitmq_with_redis_connection(
-    rabbitmq_container_2: "wrappers.Container",
-    redis_container: "wrappers.Container",
-) -> Repid:
-    while not rabbitmq_container_2.ready() or not redis_container.ready():
-        sleep(0.1)
-    return Repid(
-        Connection(
-            RabbitMessageBroker(
-                f"amqp://user:testtest@localhost:{rabbitmq_container_2.ports['5672/tcp'][0]}",
-            ),
-            RedisBucketBroker(f"redis://:test@localhost:{redis_container.ports['6379/tcp'][0]}/2"),
-            RedisBucketBroker(
-                f"redis://:test@localhost:{redis_container.ports['6379/tcp'][0]}/2",
-                use_result_bucket=True,
-            ),
-        ),
-    )
-
-
-@pytest.fixture(scope="session")
-def fake_repid() -> Repid:
-    return Repid(
-        Connection(
-            InMemoryMessageBroker(),
-            InMemoryBucketBroker(),
-            InMemoryBucketBroker(use_result_bucket=True),
-        ),
-    )
+    # server = RedisServer(f"redis://:test@localhost:{redis_container.ports['6379/tcp'][0]}/0")
+    app = Repid()
+    # app.servers.register_server("default", server, is_default=True)
+    return app  # noqa: RET504
 
 
 @pytest.fixture(
     scope="session",
     params=[
         lazy_fixture("rabbitmq_connection"),
-        lazy_fixture("redis_connection"),
-        lazy_fixture("rabbitmq_with_redis_connection"),
+        # lazy_fixture("redis_connection"),
     ],
 )
 def autoconn(request: pytest.FixtureRequest) -> Any:
-    return request.param
-
-
-@pytest.fixture(
-    scope="session",
-    params=[
-        lazy_fixture("fake_repid"),
-        lazy_fixture("redis_connection"),
-        lazy_fixture("rabbitmq_with_redis_connection"),
-    ],
-)
-def autoconn_with_buckets(request: pytest.FixtureRequest) -> Any:
     return request.param

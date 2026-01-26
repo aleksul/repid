19f54c18fb840c69b73ed1be7d9ee9a991d6f0da
aleksul <me@aleksul.space>
2026-01-26 12:49:05 +0100

    🐛 Fix a bug where AMQP server was not importable due to aiormq missing

--- repid/connections/__init__.py
+++ repid/connections/__init__.py
@@ -1,17 +1,14 @@
 from repid._utils import is_installed
 from repid.connections.abc import ServerT
+from repid.connections.amqp import AmqpServer
 from repid.connections.in_memory import InMemoryServer
 
 __all__ = [
+    "AmqpServer",
     "InMemoryServer",
     "ServerT",
 ]
 
-
-if is_installed("aiormq"):
-    from repid.connections.amqp import AmqpServer
-
-    __all__ += ["AmqpServer"]
 
 if is_installed("redis"):
     from repid.connections.redis import RedisServer
